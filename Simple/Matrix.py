9de73b2139ad127572b1700cdb1377ac31ee22e1
Sriram4n <sriram.naikwade@gmail.com>
2025-11-04 21:54:10 +0530

    error!

--- Simple/Matrix.py
+++ Simple/Matrix.py
@@ -1,8 +1,16 @@
-Matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
+# Matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+rows = int(input("Enter no. of rows: "))
+col = int(input("Enter no. of col: "))
+Matrix = []
+for i in range(rows):
+    templist = []
+    for j in range(col):
+        templist.append(input(f"Enter value of {i},{j}: "))
+    Matrix.append(templist)
 print("Matrix is: ")
 for rows in Matrix:
     for col in rows:
