6648db42b9d5dd3b5604da8b06e5a9d8fa602c8f
Sriram4n <sriram.naikwade@gmail.com>
2025-11-04 18:49:47 +0530

    update

---
+++ Simple/listAndTupleToArray.py
@@ -0,0 +1,11 @@
+import numpy as np
+my_list = [1,2,3]
+my_tuple = (1,2,3)
+
+list_array = np.array(my_list)
+tuple_array = np.array(my_tuple)
+
+print("array from list")
+print(list_array)
+print("array from tuple")
+print(tuple_array)
